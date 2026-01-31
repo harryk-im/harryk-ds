f473bfb43706d7d86610b89d32ff2775602fd999
harryk-im <hanyugeon@naver.com>
2026-01-31 22:49:57 +0900

    refactor: global로 적용되는 유틸을 global.css.ts 으로 모듈화

--- packages/ui/src/styles/foundation/global.css.ts
+++ packages/ui/src/styles/foundation/global.css.ts
@@ -1,5 +1,18 @@
-import { globalStyle } from "@vanilla-extract/css";
-import { FONT_FAMILY } from "./fonts.css";
+import { globalFontFace, globalStyle } from "@vanilla-extract/css";
+
+export const FONT_FAMILY = "Pretendard";
+
+globalFontFace(FONT_FAMILY, {
+  src: 'local("Pretendard Regular"), url("../../assets/Pretendard-Regular.woff2") format("woff2")',
+  fontWeight: 400,
+  fontDisplay: "swap",
+});
+
+globalFontFace(FONT_FAMILY, {
+  src: 'local("Pretendard SemiBold"), url("../../assets/Pretendard-SemiBold.woff2") format("woff2")',
+  fontWeight: 600,
+  fontDisplay: "swap",
+});
 
 globalStyle(":root, body, button, input, select, textarea", {
   fontFamily: FONT_FAMILY,
