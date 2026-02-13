dae3a558e4e6e4e2b906caabf1f0200aff958cdd
harryk-im <hanyugeon@naver.com>
2026-02-13 15:55:08 +0900

    feat: Paragraph 컴포넌트 타입 작성 (.types.ts)

---
+++ packages/ui/src/components/paragraph/paragraph.types.ts
@@ -0,0 +1,35 @@
+import type { RecipeVariants } from "@vanilla-extract/recipes";
+import type { AnchorHTMLAttributes, HTMLAttributes } from "react";
+import type { paragraphStyle } from "./paragraph.css";
+
+type ParagraphVariants = RecipeVariants<typeof paragraphStyle>;
+
+export type ParagraphSize = NonNullable<ParagraphVariants>["size"];
+export type ParagraphColor = NonNullable<ParagraphVariants>["color"];
+export type ParagraphBold = NonNullable<ParagraphVariants>["bold"];
+
+export interface ParagraphContextType {
+  size?: ParagraphSize;
+  color?: ParagraphColor;
+  bold?: boolean;
+}
+
+export interface BaseParagraphProps {
+  size?: ParagraphSize;
+  color?: ParagraphColor;
+  bold?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export interface ParagraphProps
+  extends Omit<HTMLAttributes<HTMLParagraphElement>, "color">,
+    BaseParagraphProps {}
+
+export interface ParagraphTextProps
+  extends Omit<HTMLAttributes<HTMLSpanElement>, "color">,
+    BaseParagraphProps {}
+
+export interface ParagraphLinkProps
+  extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "color">,
+    BaseParagraphProps {}
