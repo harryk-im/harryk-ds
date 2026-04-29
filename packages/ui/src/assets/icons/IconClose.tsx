1fc56c1a1d7aba5bfbbaa47d47ab91ab82e416c8
harryk-im <hanyugeon@naver.com>
2026-04-29 18:37:17 +0900

    feat: asset/icons/IconClose 컴포넌트 작성

---
+++ packages/ui/src/assets/icons/IconClose.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { COLORS } from "../../styles";
+
+type IconProps = {
+  color?: string;
+  size?: number;
+};
+
+export const IconClose = React.forwardRef<SVGElement, IconProps>(
+  ({ color = COLORS.black, size = 24, ...props }) => (
+    <svg
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className="line-icon"
+      {...props}
+    >
+      <path
+        fill={color}
+        fill-rule="evenodd"
+        d="M13.815 12l5.651-5.651a1.2 1.2 0 00-1.697-1.698l-5.651 5.652-5.652-5.652a1.201 1.201 0 00-1.697 1.698L10.421 12l-5.652 5.651a1.202 1.202 0 00.849 2.049c.307 0 .614-.117.848-.351l5.652-5.652 5.651 5.652a1.198 1.198 0 001.697 0 1.2 1.2 0 000-1.698L13.815 12z"
+      />
+    </svg>
+  )
+);
