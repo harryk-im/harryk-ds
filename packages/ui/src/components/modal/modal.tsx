3132ffda4f8903be268cfb631e9a3f67e47bc7aa
harryk-im <hanyugeon@naver.com>
2026-04-29 18:00:40 +0900

    docs: Modal 컴포넌트 JSDoc 문서화

--- packages/ui/src/components/modal/modal.tsx
+++ packages/ui/src/components/modal/modal.tsx
@@ -18,6 +18,24 @@ import type {
   ModalProps,
 } from "./modal.types";
 
+/**
+ * 사용자에게 중요한 정보를 전달하거나 상호작용을 유도할 때 사용하는 모달 컴포넌트예요.
+ * Controlled Component로 동작하며, `createPortal`을 통해 body 하단에 렌더링돼요.
+ *
+ * @example
+ * ```tsx
+ * const { isOpen, onOpen, onClose } = useDisclosure();
+ *
+ * <Button onClick={onOpen}>모달 열기</Button>
+ * <Modal isOpen={isOpen} onClose={onClose} size="md">
+ *   <Modal.Header onClose={onClose}>안내</Modal.Header>
+ *   <Modal.Body>내용이 들어갑니다.</Modal.Body>
+ *   <Modal.Footer>
+ *     <Button onClick={onClose}>확인</Button>
+ *   </Modal.Footer>
+ * </Modal>
+ * ```
+ */
 const ModalRoot = React.forwardRef<HTMLDivElement, ModalProps>(
   ({ isOpen, onClose, size = "md", children, ...props }, ref) => {
     useCloseOnEsc(isOpen, onClose);
@@ -41,6 +59,9 @@ const ModalRoot = React.forwardRef<HTMLDivElement, ModalProps>(
 
 ModalRoot.displayName = "Modal";
 
+/**
+ * 모달의 상단 영역으로 제목과 닫기 버튼을 포함해요.
+ */
 const ModalHeader = React.forwardRef<HTMLDivElement, ModalHeaderProps>(
   ({ onClose, className, children, ...props }, ref) => {
     const classes = [modalHeaderStyle, className].filter(Boolean).join(" ");
@@ -60,6 +81,9 @@ const ModalHeader = React.forwardRef<HTMLDivElement, ModalHeaderProps>(
 
 ModalHeader.displayName = "Modal.Header";
 
+/**
+ * 모달의 본문 영역이에요.
+ */
 const ModalBody = React.forwardRef<HTMLDivElement, ModalBodyProps>(
   ({ className, children, ...props }, ref) => {
     const classes = [modalBodyStyle, className].filter(Boolean).join(" ");
@@ -74,6 +98,9 @@ const ModalBody = React.forwardRef<HTMLDivElement, ModalBodyProps>(
 
 ModalBody.displayName = "Modal.Body";
 
+/**
+ * 모달의 하단 영역으로 주로 액션 버튼들을 배치해요.
+ */
 const ModalFooter = React.forwardRef<HTMLDivElement, ModalFooterProps>(
   ({ className, children, ...props }, ref) => {
     const classes = [modalFooterStyle, className].filter(Boolean).join(" ");
@@ -88,6 +115,9 @@ const ModalFooter = React.forwardRef<HTMLDivElement, ModalFooterProps>(
 
 ModalFooter.displayName = "Modal.Footer";
 
+/**
+ * Modal 컴포넌트예요. Header, Body, Footer를 조합하여 사용해요.
+ */
 export const Modal = Object.assign(ModalRoot, {
   Header: ModalHeader,
   Body: ModalBody,
