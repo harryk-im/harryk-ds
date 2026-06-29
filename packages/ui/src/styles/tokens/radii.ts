4d43e2134a8022bee97c3a4ec01d6bc5bf8bd7e8
harryk-im <hanyugeon@naver.com>
2026-06-29 20:14:04 +0900

    feat: radii(border-radius), spacing(padding) 토큰 정의

---
+++ packages/ui/src/styles/tokens/radii.ts
@@ -0,0 +1,8 @@
+export const RADII = {
+  none: "0",
+  xs: "6px",
+  sm: "12px",
+  md: "18px",
+  lg: "24px",
+  full: "9999px",
+} as const;
---
+++ packages/ui/src/styles/tokens/spacing.ts
@@ -0,0 +1,8 @@
+export const SPACING = {
+  none: "0",
+  xs: "6px",
+  sm: "12px",
+  md: "18px",
+  lg: "24px",
+  xl: "30px",
+} as const;
